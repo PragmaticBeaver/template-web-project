8468538a5247227a98cca7dd3cfa848998951e5c
Beaver <8586904+PragmaticBeaver@users.noreply.github.com>
2022-09-13 14:58:49 +0200

    chore: initial project setup
    Added eslint & prettier (with config), esbuild (with build script), lite-server (with config) and bare bones webmanifest, basic VS Code config

---
+++ build/build.js
@@ -0,0 +1,29 @@
+import { build } from "esbuild";
+import fs from "fs";
+import path from "path";
+
+const srcPath = "./src/";
+const distDir = "./dist";
+
+// create dist directory
+if (!fs.existsSync(distDir)) {
+  fs.mkdirSync(distDir);
+}
+
+// copy HTML template
+fs.copyFileSync("./index.html", distDir + "/index.html");
+
+// copy css
+fs.copyFileSync(srcPath + "/index.css", distDir + "/index.css");
+
+// copy web manifest
+fs.copyFileSync("./manifest.webmanifest", distDir + "/manifest.webmanifest");
+
+// build
+const entry = path.join(srcPath, "index.js");
+build({
+  entryPoints: [entry],
+  bundle: true,
+  loader: { ".svg": "dataurl" },
+  outdir: distDir,
+}).catch(() => process.exit(1));
